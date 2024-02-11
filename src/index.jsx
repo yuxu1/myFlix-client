54332f7941790125f3d8e73a462932119cccf034
Yufan <yuxu1@users.noreply.github.com>
2024-02-10 20:33:38 -0800

    wrapped app in a Container component

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import { createRoot} from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 //import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 //main component
 const App = () => {
-    return <MainView />;
+    return 
+    <Container>
+        <MainView />
+    </Container>
 };
 
 //finds the root of your app
